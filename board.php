435260793955cd0b699f478864317f15e856f97e
downy <ekdnsl086@gmail.com>
2019-04-10 17:05:57 +0900

    board

---
+++ board.php
@@ -0,0 +1,16 @@
+<?php include("header.php"); ?>
+<!--개인연결-->
+		<!-- <link rel="stylesheet" type="text/css" media="screen" href="./css/register.css">  -->
+<?php include("nav.php"); ?>
+		
+    <script>		
+		
+	</script>
+
+    <div id="section">
+        <div class="container">			
+            <h3>공지사항</h3>
+            
+        </div>
+
+  <?php include("footer.php"); ?>
